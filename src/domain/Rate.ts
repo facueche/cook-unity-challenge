0fc7c56f9de0d450660591104e87635b2642a1d7
Facu Echenique <facueche@gmail.com>
2022-11-10 13:06:07 -0300

    feat: implements rate creation core

---
+++ src/domain/Rate.ts
@@ -0,0 +1,59 @@
+import Meal from "./Meal";
+import User from "./User";
+
+export default class Rate
+{
+    private uuid: string = "";
+    private customer: User;
+    private meal: Meal;
+    private rate: number = 0;
+
+    private constructor(uuid: string, customer: User, meal: Meal, rate: number)
+    {
+        this.assertUuidIsValid(uuid);
+        this.assertRateIsValid(rate);
+        this.customer = customer;
+        this.meal = meal;
+    }
+
+    private assertUuidIsValid(uuid: string)
+    {
+        if (uuid == "" || uuid === undefined)
+            throw new Error("Uuid is required");
+
+        this.uuid = uuid;
+    }
+
+    private assertRateIsValid(rate: number)
+    {
+        if (isNaN(rate) || rate < 1 || rate >5)
+            throw new Error("Rate is invalid");
+
+        this.rate = rate;
+    }
+
+    public static register(uuid: string, customer: User, meal: Meal, rate: number): Rate
+    {
+        return new Rate(uuid, customer, meal, rate);
+    }
+
+    public getUuid(): string
+    {
+        return this.uuid;
+    }
+
+    public getCustomer(): User
+    {
+        return this.customer;
+    }
+
+    public getMeal(): Meal
+    {
+        return this.meal;
+    }
+
+    public getRate(): number
+    {
+        return this.rate;
+    }
+}
---
+++ test/unit/rate.test.ts
@@ -0,0 +1,67 @@
+import Meal from "../../src/domain/Meal";
+import Rate from "../../src/domain/Rate";
+import Role from "../../src/domain/Role";
+import User from "../../src/domain/User";
+
+describe("Rating registration", () => {
+    it("Uuid is required", () => {
+        let tempRate = { ...rate };
+        tempRate.uuid = "";
+
+        expect(() => Rate.register(tempRate.uuid, tempRate.customer, tempRate.meal, tempRate.rate)).toThrowError("Uuid is required");
+    });
+
+    it("Rate is required", () => {
+        let tempRate = { ...rate };
+        tempRate.rate = Number.NaN;
+
+        expect(() => Rate.register(tempRate.uuid, tempRate.customer, tempRate.meal, tempRate.rate)).toThrowError("Rate is invalid");
+    });
+
+    it("Rate should be between 1 and 5", () => {
+        let tempRate = { ...rate };
+        tempRate.rate = 0;
+
+        expect(() => Rate.register(tempRate.uuid, tempRate.customer, tempRate.meal, tempRate.rate)).toThrowError("Rate is invalid");
+
+        tempRate.rate = 6;
+
+        expect(() => Rate.register(tempRate.uuid, tempRate.customer, tempRate.meal, tempRate.rate)).toThrowError("Rate is invalid");
+    });
+
+    it("Rate should be registered with correct data", () => {
+        const tempRate: Rate = Rate.register(rate.uuid, rate.customer, rate.meal, rate.rate);
+
+        expect(tempRate.getUuid()).toBe(rate.uuid);
+        expect(tempRate.getCustomer()).toBe(rate.customer);
+        expect(tempRate.getMeal()).toBe(rate.meal);
+        expect(tempRate.getRate()).toBe(rate.rate);
+    });
+});
+
+const customer = User.register(
+    "123e4567-e89b-12d3-a456-426614174001",
+    "customer",
+    "secret",
+    Role.CUSTOMER
+);
+
+const chef = User.register(
+    "123e4567-e89b-12d3-a456-426614174002",
+    "chefsito",
+    "secret",
+    Role.CHEF
+);
+
+const meal = Meal.register(
+    "123e4567-e89b-12d3-a456-426614174003",
+    "EggplantParmesan",
+    chef
+);
+
+const rate = {
+    uuid: "123e4567-e89b-12d3-a456-426614174004",
+    customer,
+    meal,
+    rate: 5, // stars
+}
