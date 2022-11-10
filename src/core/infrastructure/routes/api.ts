37e565796ab063ad027cf9a96ff97c7a7b362256
Facu Echenique <facueche@gmail.com>
2022-11-10 15:56:47 -0300

    chore: controllers folder reesctructured

--- src/core/infrastructure/routes/api.ts
+++ src/core/infrastructure/routes/api.ts
@@ -1,8 +1,8 @@
 import { Application, Router } from "express";
-import ChefLoginController from "../controllers/ChefLogin.controller";
-import ChefRegisterController from "../controllers/ChefRegister.controller";
-import CustomerLoginController from "../controllers/CustomerLogin.controller";
-import CustomerRegisterController from "../controllers/CustomerRegister.controller";
+import ChefLoginController from "../controllers/auth/ChefLogin.controller";
+import ChefRegisterController from "../controllers/auth/ChefRegister.controller";
+import CustomerLoginController from "../controllers/auth/CustomerLogin.controller";
+import CustomerRegisterController from "../controllers/auth/CustomerRegister.controller";
 
 export const loadApiEndpoints = (app: Application): void => {
     app.use("/chef", chefRouter);
