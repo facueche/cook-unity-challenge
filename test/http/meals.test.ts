b4f55f2547039584c91485147fbdab4dded2f71b
Facu Echenique <facueche@gmail.com>
2022-11-10 19:27:09 -0300

    test: add empty tests for meals requests

---
+++ test/http/meals.test.ts
@@ -0,0 +1,38 @@
+import request from "supertest";
+import app from "../../src/app";
+
+describe("Chef", () => {
+    it("should create meals", async () => {
+        //
+    });
+
+    it("Should see list of meals with rate", () => {
+        //
+    });
+
+    it("Should not rate a meal", () => {
+        //
+    })
+});
+
+describe("Customer", () => {
+    it("Should not create meals", () => {
+
+    });
+
+    it("should see list of meals", () => {
+        //
+    });
+
+    it("Should filter list of meals by chef", () => {
+        //
+    });
+
+    it("Should rate meals", () => {
+        //
+    });
+
+    it("Should not rate meals more than once", () => {
+        //
+    });
+});
